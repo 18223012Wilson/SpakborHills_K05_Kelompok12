d0c5dad665f8ae1f81796a24413f5ce82815d8bc
vincentiabelindaa <18223078@std.stei.itb.ac.id>
2025-05-10 19:47:09 +0700

    Create Point.java

---
+++ ITEMS/Point.java
@@ -0,0 +1,20 @@
+// buat location fish
+
+package ITEMS;
+public class Point {
+    private int x = 0;
+    private int y = 0;
+    
+    public Point(int a, int b) {
+        x = a; 
+        y = b;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+} 
